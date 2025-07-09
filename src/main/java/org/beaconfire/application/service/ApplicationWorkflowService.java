c47904595e3d01557c975e511953f0ac5c937ef3
danielqq000 <danielqq000@yahoo.com>
2025-07-09 18:22:54 -0500

    remove service files

--- src/main/java/org/beaconfire/application/service/ApplicationWorkflowService.java
+++
@@ -1,9 +0,0 @@
-package org.beaconfire.application.service;
-
-import org.beaconfire.application.dto.ApplicationWorkflowRequestDTO;
-import org.beaconfire.application.entity.ApplicationWorkFlow;
-
-
-public interface ApplicationWorkflowService {
-    ApplicationWorkFlow saveApplicationWorkflow(ApplicationWorkflowRequestDTO dto);
-}
